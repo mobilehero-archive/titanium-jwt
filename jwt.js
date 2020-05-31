49c5877d6bf32540e15a3ae2e1f150512aa1466b
Brenton House <brenton.house@gmail.com>
2020-05-31 15:37:07 -0500

    udpates

--- jwt.js
+++ jwt.js
@@ -11,7 +11,7 @@
  * Module dependencies.
  */
 const crypto = require('crypto');
-
+const timespan = require('./timespan');
 
 /**
  * Support algorithm mapping.
@@ -34,6 +34,23 @@ const typeMap = {
 };
 
 
+const options_to_payload = {
+	audience: 'aud',
+	issuer:   'iss',
+	subject:  'sub',
+	jwtid:    'jti',
+};
+
+const options_for_objects = [
+	'expiresIn',
+	'notBefore',
+	'noTimestamp',
+	'audience',
+	'issuer',
+	'subject',
+	'jwtid',
+];
+
 /**
  * Expose object.
  */
@@ -52,7 +69,7 @@ jwt.version = '0.5.6';
  * @param {string} key
  * @param {boolean} [noVerify]
  * @param {string} [algorithm]
- * @returns {object} payload
+ * @returns {object} Payload.
  * @api public
  */
 jwt.decode = function jwt_decode(token, key, noVerify, algorithm) {
@@ -113,19 +130,22 @@ jwt.decode = function jwt_decode(token, key, noVerify, algorithm) {
  * @param {object} payload
  * @param {string} key
  * @param {string} algorithm
+ * @param callback
+ * @param secretOrPrivateKey
  * @param {object} options
- * @returns {string} token
+ * @returns {string} Token.
  * @api public
  */
-jwt.sign = function jwt_encode(payload, key, algorithm, options = {}) {
-	// Check key
-	if (!key) {
-		throw new Error('Require key');
-	}
+jwt.sign = function jwt_encode(payload, secretOrPrivateKey, { algorithm = 'HS256', expiresIn, notBefore, audience, issuer, jwtid, subject, noTimestamp, header, keyid } = {}, callback) {
 
-	// Check algorithm, default is HS256
-	if (!options.algorithm) {
-		options.algorithm = 'HS256';
+	const options = { algorithm, expiresIn, notBefore, audience, issuer, jwtid, subject, noTimestamp, header, keyid };
+
+	// DEBUG: options
+	console.debug(`🦠  options: ${JSON.stringify(options, null, 2)}`);
+
+	// Check key
+	if (!secretOrPrivateKey) {
+		throw new Error('Require key');
 	}
 
 	const signingMethod = algorithmMap[options.algorithm];
@@ -133,18 +153,79 @@ jwt.sign = function jwt_encode(payload, key, algorithm, options = {}) {
 	if (!signingMethod || !signingType) {
 		throw new Error('Algorithm not supported');
 	}
+	const isObjectPayload = typeof payload === 'object'
+	&& !Buffer.isBuffer(payload);
 
-	// header, typ is fixed value.
-	const header = { typ: 'JWT', alg: options.algorithm };
-	if (options && options.header) {
-		assignProperties(header, options.header);
-	}
+	header = Object.assign({
+		alg: options.algorithm || 'HS256',
+		typ: isObjectPayload ? 'JWT' : undefined,
+		kid: keyid,
+	 }, header);
+
+	 function failure(err) {
+		if (callback) {
+		  return callback(err);
+		}
+		throw err;
+	 }
+
+	 if (!secretOrPrivateKey && options.algorithm !== 'none') {
+		return failure(new Error('secretOrPrivateKey must have a value'));
+	 }
+
+	 if (typeof payload.exp !== 'undefined' && typeof options.expiresIn !== 'undefined') {
+		return failure(new Error('Bad "expiresIn" option the payload already has an "exp" property.'));
+	 }
+
+	 if (typeof payload.nbf !== 'undefined' && typeof options.notBefore !== 'undefined') {
+		return failure(new Error('Bad "notBefore" option the payload already has an "nbf" property.'));
+	 }
+
+	 const timestamp = payload.iat || Math.floor(Date.now() / 1000);
+
+	 if (noTimestamp) {
+		delete payload.iat;
+	 } else if (isObjectPayload) {
+		payload.iat = timestamp;
+	 }
+
+	 if (typeof notBefore !== 'undefined') {
+		try {
+		  payload.nbf = timespan(notBefore, timestamp);
+		} catch (err) {
+		  return failure(err);
+		}
+		if (typeof payload.nbf === 'undefined') {
+		  return failure(new Error('"notBefore" should be a number of seconds or string representing a timespan eg: "1d", "20h", 60'));
+		}
+	 }
+
+	 if (typeof options.expiresIn !== 'undefined' && typeof payload === 'object') {
+		try {
+		  payload.exp = timespan(options.expiresIn, timestamp);
+		} catch (err) {
+		  return failure(err);
+		}
+		if (typeof payload.exp === 'undefined') {
+		  return failure(new Error('"expiresIn" should be a number of seconds or string representing a timespan eg: "1d", "20h", 60'));
+		}
+	 }
+
+	 Object.keys(options_to_payload).forEach(key => {
+		const claim = options_to_payload[key];
+		if (typeof options[key] !== 'undefined') {
+		  if (typeof payload[claim] !== 'undefined') {
+			 return failure(new Error(`Bad "options.${key}" option. The payload already has an "${claim}" property.`));
+		  }
+		  payload[claim] = options[key];
+		}
+	 });
 
 	// create segments, all segments should be base64 string
 	const segments = [];
 	segments.push(base64urlEncode(JSON.stringify(header)));
 	segments.push(base64urlEncode(JSON.stringify(payload)));
-	segments.push(sign(segments.join('.'), key, signingMethod, signingType));
+	segments.push(sign(segments.join('.'), secretOrPrivateKey, signingMethod, signingType));
 
 	return segments.join('.');
 };
@@ -153,13 +234,13 @@ jwt.sign = function jwt_encode(payload, key, algorithm, options = {}) {
  * Private util functions.
  */
 
-function assignProperties(dest, source) {
-	for (const attr in source) {
-		if (source.hasOwnProperty(attr)) {
-			dest[attr] = source[attr];
-		}
-	}
-}
+// function assignProperties(dest, source) {
+// 	for (const attr in source) {
+// 		if (source.hasOwnProperty(attr)) {
+// 			dest[attr] = source[attr];
+// 		}
+// 	}
+// }
 
 function verify(input, key, method, type, signature) {
 	if (type === 'hmac') {
